dd1dbf8bf79510ca05aa21040523b50fd7e15c5a
Daniel Garcia <danigb92@hotmail.com>
2019-10-21 09:36:10 +0200

    C8 terminado

---
+++ VisualBasic/C8/fC8.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class fC8
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblResul = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblResul
+        '
+        Me.lblResul.AutoSize = True
+        Me.lblResul.Location = New System.Drawing.Point(12, 9)
+        Me.lblResul.Name = "lblResul"
+        Me.lblResul.Size = New System.Drawing.Size(0, 13)
+        Me.lblResul.TabIndex = 0
+        '
+        'fC8
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(306, 200)
+        Me.Controls.Add(Me.lblResul)
+        Me.Name = "fC8"
+        Me.Text = "Ejercicio C8"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblResul As Label
+End Class
---
+++ VisualBasic/C8/fC8.vb
@@ -0,0 +1,35 @@
+﻿Public Class fC8
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
+
+        Dim array(9) As Integer
+        Dim duplicados As Integer = 0
+        Dim positivos As Integer = 0
+        Dim negativos As Integer = 0
+        Dim ceros As Integer = 0
+
+        For i = 0 To 9
+            array(i) = CInt((Rnd() * Math.Pow(-1, CInt(Rnd() * 10))) * 9)
+            lblResul.Text &= array(i) & "   "
+        Next
+        For i = 0 To 9
+            If array(i) < 0 Then
+                negativos += 1
+            ElseIf array(i) > 0 Then
+                positivos += 1
+            Else
+                ceros += 1
+            End If
+        Next
+
+        Dim arrayUnicos = array.Distinct()
+        For Each num As Integer In arrayUnicos
+            If array.Count(Function(numero) numero = num) > 1 Then duplicados += 1
+        Next
+
+        lblResul.Text &= vbNewLine & vbNewLine & "Positivos: " & positivos &
+            vbNewLine & "Ceros: " & ceros &
+            vbNewLine & "Negativos: " & negativos &
+            vbNewLine & "Duplicados: " & duplicados
+    End Sub
+End Class
