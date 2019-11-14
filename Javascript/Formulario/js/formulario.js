bea0f87add8dc8c73700a9301613a1f5822948fb
Daniel Garcia <danigb92@hotmail.com>
2019-11-14 10:25:21 +0100

    formulario 2.0

--- Javascript/Formulario/js/formulario.js
+++ Javascript/Formulario/js/formulario.js
@@ -1,7 +1,24 @@
 "use strict";
 
 function sexo() {
-    let sexo=document.getElementsByName('genero');
-    alert(sexo);
-    console.log(sexo);
+    let sexo = document.getElementsByName('genero');
+
+    sexo.forEach((a, b) => {
+        if (a.checked) {
+            document.getElementById("sexo").innerHTML = a.value;
+        }
+    })
+
+    let salida=""
+
+    let vehi=document.getElementsByName("vehiculo");
+    //let vehi = document.querySelectorAll("input[name='vehiculo'])
+
+    vehi.forEach(value =>{
+        if (value.checked){
+            salida +=value.value +"<br>";
+        }
+    })
+
+    document.getElementById("vehiculo").innerHTML = salida;
 }
