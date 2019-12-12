99af6fa39036fa2ab1ce958ef07c97222c7dfd56
Drane8 <danigb92@hotmail.com>
2019-12-12 21:14:27 +0100

    ordenado array

--- Javascript/Ejercicio 18/js/main.js
+++ Javascript/Ejercicio 18/js/main.js
@@ -4,4 +4,5 @@ let numeros = [];
 for(let i = 0; i < 6; i++){
     numeros.push(Math.floor(Math.random() * (49)) + 1);
 }
+numeros.sort((a, b)=>{return a-b});
 document.getElementById("resul").innerHTML = numeros;
