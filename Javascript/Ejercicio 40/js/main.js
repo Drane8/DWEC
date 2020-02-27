bd01270282df3f8f2a7a6799d4964f078c97e7e3
Daniel Garcia <danigb92@hotmail.com>
2020-02-27 09:17:26 +0100

    ejercicios2ªeva hasta el 40

--- Javascript/Ejercicio 40/js/main.js
+++ Javascript/Ejercicio 40/js/main.js
@@ -14,17 +14,19 @@ let formEmpresa = "<form id='f_emp'>\n" +
     "            </form>";
 
 let datosInsertados = false;
-let formEmpAbierto = false;
+let formAbierto = false;
 let empresitas = [];
 
 function crearCheckbox(nombre) {
-    $("#radios").append("<li id='li"+nombre+"'><label for='" + nombre + "'>" + nombre + "</label>" +
+    $("#radios").append("<li id='li" + nombre + "'><label for='" + nombre + "'>" + nombre + "</label>" +
         "<input type='radio' name='radChe' id='" + nombre + "' value='" + nombre + "'/></li>");
     $("#" + nombre).checkboxradio();
 }
 
 $(document).ready(function () {
     $("#radios").sortable();
+
+    $("#menu").sortable();
 
     $('#menu').menu();
 
@@ -50,68 +52,39 @@ $(document).ready(function () {
             if (radioValue === "Datos") {
                 datosInsertados = false;
             }
+            if(empresitas.includes(radioValue)){
+                empresitas.splice(empresitas.indexOf(radioValue))
+            }
         }
     });
 
     $("#visualizar").click(function () {
         let radioValue = $("input[name='radChe']:checked").val();
-        $("#dialog" + radioValue).dialog("open");
+        if (radioValue === "" || radioValue == undefined) {
+            alert("Seleccione un radio");
+        } else {
+            $("#dialog" + radioValue).dialog("open");
+        }
     });
 
     $("#datos").click(function () {
         if (!datosInsertados) {
-            $("#form").append(formDatos);
-            $("#terminar").click(function (ev) {
-                ev.preventDefault();
-                let usuario = $("#user").val();
-                let pass = $("#pass").val();
-                if (usuario === "" || pass === "") {
-                    alert("Rellene todos los campos")
-                } else {
-                    crearCheckbox("Datos");
-                    datosInsertados = true;
-                    $("body").append("<div id=\"dialogDatos\" title=\"Datos\">\n" +
-                        "Nombre: " + usuario + "<br/>Pass: " + pass + "   \n" +
-                        "</div>");
-                    $("#dialogDatos").dialog({
-                        autoOpen: false,
-                        show: {
-                            effect: "blind",
-                            duration: 1000
-                        },
-                        hide: {
-                            effect: "explode",
-                            duration: 1000
-                        }
-                    });
-                    $("#f_datos").remove();
-                }
-
-            });
-        } else {
-            alert("Ya hay datos insertados");
-        }
-    });
-
-    $("#empresa").click(function () {
-        if (!formEmpAbierto) {
-            formEmpAbierto = true;
-            $("#form").append(formEmpresa);
-            $("#terminarEm").click(function (ev) {
+            if (!formAbierto) {
+                formAbierto = true;
+                $("#form").append(formDatos);
+                $("#terminar").click(function (ev) {
                     ev.preventDefault();
-                    let nomEm = $("#nomEm").val();
-                    let dir = $("#dir").val();
-                    let local = $("#local").val();
-                    if (nomEm === "" || dir === "" || local === "") {
-                        alert("Rellene todos los campos");
-                    } else if (empresitas.includes(nomEm)) {
-                        alert("La empresa ya existe");
+                    let usuario = $("#user").val();
+                    let pass = $("#pass").val();
+                    if (usuario === "" || pass === "") {
+                        alert("Rellene todos los campos")
                     } else {
-                        crearCheckbox(nomEm);
-                        $("body").append("<div id=\"dialog" + nomEm + "\" title=\"Datos\">\n" +
-                            "Nombre: " + nomEm + "<br/>Direccion: " + dir + "   \n" + "<br/>Localidad: " + local + "   \n" +
+                        crearCheckbox("Datos");
+                        datosInsertados = true;
+                        $("body").append("<div id=\"dialogDatos\" title=\"Datos\">\n" +
+                            "Nombre: " + usuario + "<br/>Pass: " + pass + "   \n" +
                             "</div>");
-                        $("#dialog" + nomEm).dialog({
+                        $("#dialogDatos").dialog({
                             autoOpen: false,
                             show: {
                                 effect: "blind",
@@ -122,13 +95,157 @@ $(document).ready(function () {
                                 duration: 1000
                             }
                         });
-                        empresitas.push(nomEm);
-                        formEmpAbierto = false;
-                        $("#f_emp").remove();
+                        formAbierto = false;
+                        $("#f_datos").remove();
                     }
-                }
-            );
 
+                });
+            }
+        } else {
+            alert("Ya hay datos insertados");
+        }
+    });
+
+    $("#empresa").click(function () {
+        if (empresitas.length >= 3) {
+            alert("Se ha alcanzado el máximo de empresas (3)")
+        } else {
+            if (!formAbierto) {
+                formAbierto = true;
+                $("#form").append(formEmpresa);
+                $("#terminarEm").click(function (ev) {
+                        ev.preventDefault();
+                        let nomEm = $("#nomEm").val();
+                        let dir = $("#dir").val();
+                        let local = $("#local").val();
+                        if (nomEm === "" || dir === "" || local === "") {
+                            alert("Rellene todos los campos");
+                        } else if (empresitas.includes(nomEm)) {
+                            alert("La empresa ya existe");
+                        } else {
+                            crearCheckbox(nomEm);
+                            $("body").append("<div id=\"dialog" + nomEm + "\" title=\"Datos\">\n" +
+                                "Nombre: " + nomEm + "<br/>Direccion: " + dir + "   \n" + "<br/>Localidad: " + local + "   \n" +
+                                "</div>");
+                            $("#dialog" + nomEm).dialog({
+                                autoOpen: false,
+                                show: {
+                                    effect: "blind",
+                                    duration: 1000
+                                },
+                                hide: {
+                                    effect: "explode",
+                                    duration: 1000
+                                }
+                            });
+                            empresitas.push(nomEm);
+                            formAbierto = false;
+                            $("#f_emp").remove();
+                        }
+                    }
+                );
+
+            }
+        }
+    });
+
+    $("#empresa").click(function () {
+        if (empresitas.length >= 3) {
+            alert("Se ha alcanzado el máximo de empresas (3)")
+        } else {
+            if (!formAbierto) {
+                formAbierto = true;
+                $("#form").append(formEmpresa);
+                $("#terminarEm").click(function (ev) {
+                        ev.preventDefault();
+                        let nomEm = $("#nomEm").val();
+                        let dir = $("#dir").val();
+                        let local = $("#local").val();
+                        if (nomEm === "" || dir === "" || local === "") {
+                            alert("Rellene todos los campos");
+                        } else if (empresitas.includes(nomEm)) {
+                            alert("La empresa ya existe");
+                        } else {
+                            crearCheckbox(nomEm);
+                            $("body").append("<div id=\"dialog" + nomEm + "\" title=\"Empresa\">\n" +
+                                "Nombre: " + nomEm + "<br/>Direccion: " + dir + "   \n" + "<br/>Localidad: " + local + "   \n" +
+                                "</div>");
+                            $("#dialog" + nomEm).dialog({
+                                autoOpen: false,
+                                show: {
+                                    effect: "blind",
+                                    duration: 1000
+                                },
+                                hide: {
+                                    effect: "explode",
+                                    duration: 1000
+                                }
+                            });
+                            empresitas.push(nomEm);
+                            formAbierto = false;
+                            $("#f_emp").remove();
+                        }
+                    }
+                );
+
+            }
+        }
+    });
+
+    $("#organizacion").click(function () {
+        if (empresitas.length < 1) {
+            alert("No hay empresas introducidas")
+        } else {
+            if (!formAbierto) {
+                formAbierto = true;
+
+                let formulario = "<form id='f_org'>\n" +
+                    "                <label for=\"nomOrg\">Nombre</label><input type=\"text\" id=\"nomOrg\"/><br/>\n" +
+                    "                <label for=\"pais\">País</label><input type=\"text\" id=\"pais\"/><br/>\n" +
+                    "<select id='empresas' multiple>";
+                    for(let empresita of empresitas){
+                        formulario += "<option value='" + empresita + "'>" + empresita + "</option>";
+                    }
+
+
+                     formulario +=" </select><button id=\"terminarOrg\">Terminar</button>\n" +  "</form>";
+
+                $("#form").append(formulario);
+                $("#terminarOrg").click(function (ev) {
+                        ev.preventDefault();
+
+
+                        let nomOrg = $("#nomOrg").val();
+                        let pais = $("#pais").val();
+                        let empresas = $("#empresas").val();
+                        if (nomOrg === "" || pais === "" || empresas.length === 0) {
+                            alert("Rellene todos los campos");
+                        } else if (empresitas.includes(nomOrg)) {
+                            alert("No puede tener el mismo nombre que una empresa");
+                        }else {
+                            crearCheckbox(nomOrg);
+                            let dialog = "<div id=\"dialog" + nomOrg + "\" title=\"Organizacion\">\n" +
+                                "Nombre: " + nomOrg + "<br/>Pais: " + pais + "<br/>Empresas: " + empresas + "   \n" +
+                                "</div>";
+                            $("body").append(dialog);
+                            $("#dialog" + nomOrg).dialog({
+                                autoOpen: false,
+                                show: {
+                                    effect: "blind",
+                                    duration: 1000
+                                },
+                                hide: {
+                                    effect: "explode",
+                                    duration: 1000
+                                }
+                            });
+                            formAbierto = false;
+                            $("#f_org").remove();
+                        }
+                    }
+                );
+
+            }
         }
     });
 
