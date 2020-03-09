b6157999412ac92d02f93e32b11eb58f349b03ee
Daniel Garcia <danigb92@hotmail.com>
2020-03-09 08:56:23 +0100

    Ejercicio 46 (OCA) terminado

--- Javascript/Ejercicio 46/js/main.js
+++ Javascript/Ejercicio 46/js/main.js
@@ -1,32 +1,37 @@
 "use strict";
 
-const tamañoTablero = 12;
-let totalOcas = 2;
-let totalPozos = 2;
-let tablero = new Array(tamañoTablero);
+const tamTablero = 16;
+let totalOcas = 3;
+let totalPozos = 3;
+let tablero = new Array(tamTablero);
+
+
+let inicioPartida = false;
+let turno = 'fichaAzul';
+let fin = false;
 
 
 tablero[tablero.length - 1] = "finOca.png";
 
 for (let i = 0; i < totalOcas; i++) {
-    let random = Math.floor(Math.random() * tamañoTablero);
+    let random = Math.floor(Math.random() * tamTablero);
     do {
-        random = Math.floor(Math.random() * tamañoTablero);
+        random = Math.floor(Math.random() * tamTablero);
     } while (typeof tablero[random] !== 'undefined');
     tablero[random] = "oca.png";
 }
 
 for (let i = 0; i < totalPozos; i++) {
-    let random = Math.floor(Math.random() * tamañoTablero);
+    let random = Math.floor(Math.random() * tamTablero);
     do {
-        random = Math.floor(Math.random() * tamañoTablero);
+        random = Math.floor(Math.random() * tamTablero);
     } while (typeof tablero[random] !== 'undefined');
     tablero[random] = "pozo.gif";
 }
 
 let tabla = "<table id='tablero'><tr><td id='salida'></td>";
 
-for (let i = 0; i < tamañoTablero; i++) {
+for (let i = 0; i < tamTablero; i++) {
     tabla += "<td>" + (typeof tablero[i] !== 'undefined' ? "<img src='img/" + tablero[i] + "'/>" : '') + "</td>";
 }
 
@@ -44,36 +49,45 @@ window.addEventListener("load", () => {
     $("#contenido").append("<button id='comenzar'>Comenzar partida</button>");
 
 
-    let inicioPartida = false;
-    let turno = 'fichaAzul';
-
     function jugarTurno() {
         let dado = Math.floor(Math.random() * 6) + 1;
-        let casilla = parseInt($("#" + turno).attr('casilla')) + dado;
-        casilla = casilla > 12 ? 12 : casilla;
-        $("#" + turno).css({"-webkit-transform": "translate(" + (casilla * 63 + 10) + "px,0)"}).css('transition', 'all 2s');
-        setTimeout(()=>{switch (tablero[casilla - 1]) {
-            case 'pozo.gif':
-                console.log("pozo");
-                $("#" + turno).attr('casilla', 0);
-                casilla = 0;
-                $("#" + turno).css({"-webkit-transform": "translate(" + (casilla * 63 + 10) + "px,0)"}).css('transition', 'all 2s');
-                cambiarTurno();
-                break;
-            case 'oca.png':
-                let oca = tablero.indexOf('oca.png', casilla)+1;
-                $("#" + turno).css({"-webkit-transform": "translate(" + (oca * 63 + 10) + "px,0)"}).css('transition', 'all 2s');
-                $("#" + turno).attr('casilla', oca);
-                break;
-
-            default:
-                $("#"+turno).attr('casilla',casilla);
-                cambiarTurno();
-                break;
-        }
+        $("#resul").append("</br>Has sacado un: " + dado);
+        let ficha = $("#" + turno);
+        let casilla = parseInt(ficha.attr('casilla')) + dado;
+        casilla = casilla > tamTablero ? tamTablero - (casilla - tamTablero) : casilla;
+        ficha.css({"-webkit-transform": "translate(" + (casilla * 63 + 10) + "px,0)"}).css('transition', 'all 2s');
+        setTimeout(() => {
+            switch (tablero[casilla - 1]) {
+                case 'pozo.gif':
+                    ficha.attr('casilla', 0);
+                    casilla = 0;
+                    ficha.css({"-webkit-transform": "translate(" + (casilla * 63 + 10) + "px,0)"}).css('transition', 'all 2s');
+                    $("#resul").append("</br>Has caido en el pozo, vuelve a empezar");
+                    cambiarTurno();
+                    break;
+                case 'oca.png':
+                    let oca = tablero.indexOf('oca.png', casilla) + 1;
+                    if (oca !== 0) {
+                        ficha.css({"-webkit-transform": "translate(" + (oca * 63 + 10) + "px,0)"}).css('transition', 'all 2s');
+                        ficha.attr('casilla', oca);
+                    } else {
+                        ficha.attr('casilla', casilla);
+                    }
+                    $("#resul").append("</br>De oca a oca y tiro por que me toca");
+                    break;
+                case 'finOca.png':
+                    fin = true;
+                    break;
+                default:
+                    ficha.attr('casilla', casilla);
+                    cambiarTurno();
+                    break;
+            }
         }, 2500);
+        setTimeout(()=>{
 
-
+            $("#resul").text("Turno de: " + turno);
+        },5001)
     }
 
     function cambiarTurno() {
@@ -93,10 +107,22 @@ window.addEventListener("load", () => {
         if (!inicioPartida) {
             $(this).text("Tirar dado");
             inicioPartida = true;
+            $("#resul").text("Turno de: " + turno);
         } else {
             $(this).attr("disabled", true);
             jugarTurno();
-            setTimeout(()=>{$(this).removeAttr("disabled")},5000);
+            setTimeout(() => {
+                $(this).removeAttr("disabled")
+            }, 5001);
+            setTimeout (()=>{
+                if(fin){
+                    let ganador = "";
+                    ganador = turno === "fichaAzul" ? "azul" : "rojo";
+                    $("#resul").text("").append("Fin de la partida<br> Ha ganado el jugador "+ganador);
+                    $(this).attr("disabled", true).text("FIN PARTIDA");
+
+                }
+            },5100);
         }
 
     });
